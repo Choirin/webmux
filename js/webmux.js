a7f1b30892418ef6faa35a8c65c8206eeb610b84
Kohei Takada <khi.tkd@gmail.com>
2021-06-10 16:44:09 +0900

    Refactor.

--- js/webmux.js
+++ js/webmux.js
@@ -7,10 +7,9 @@ const SplitDirection = Object.freeze({
 });
 
 class Container {
-  constructor(parent, width, height, splitDirection) {
+  constructor(parent, size, splitDirection) {
     this.parent = parent;
-    this.width = width;
-    this.height = height;
+    this.size = size;
     this.splitDirection = splitDirection;
     this.children = [];
   }
@@ -33,19 +32,12 @@ class Container {
 
   removeChild(child) {
     let index = this.children.indexOf(child);
-    if (this.children.length > 1) {
-      if (this.splitDirection == SplitDirection.horizontal) {
-        if (index == this.children.length - 1) {
-          this.children[index - 1].height += child.height;
-        } else {
-          this.children[index + 1].height += child.height;
-        }
-      } else if (this.splitDirection == SplitDirection.vertical) {
-        if (index == this.children.length - 1) {
-          this.children[index - 1].width += child.width;
-        } else {
-          this.children[index + 1].width += child.width;
-        }
+    if (this.children.length > 1 && this.splitDirection !== null) {
+      const whIndex = this.splitDirection === SplitDirection.vertical? 0: 1;
+      if (index == this.children.length - 1) {
+        this.children[index - 1].size[whIndex] += child.size[whIndex];
+      } else {
+        this.children[index + 1].size[whIndex] += child.size[whIndex];
       }
     }
 
@@ -80,31 +72,30 @@ class Container {
   getChildPosition(child) {
     let position = this.getPosition();
     for (var i = 0; i < this.children.length; i++) {
-      if (this.children[i] === child) return position;
-      if (this.splitDirection == SplitDirection.vertical) {
-        position[0] += this.children[i].width;
-      } else if (this.splitDirection == SplitDirection.horizontal) {
-        position[1] += this.children[i].height;
-      }
+      if (this.children[i] === child || this.splitDirection === null) return position;
+      const whIndex = this.splitDirection === SplitDirection.vertical? 0: 1;
+      position[whIndex] += this.children[i].size[whIndex];
     }
     console.error('cannot find position');
   }
 
-  splitHorizontally() {
-    let newMyHeight = parseInt(this.height / 2);
-    let newOtherHeight = this.height - newMyHeight;
+  split(splitDirection) {
+    const whIndex = splitDirection === SplitDirection.vertical? 0: 1;
+    let newMySize = this.size.slice();
+    newMySize[whIndex] = parseInt(newMySize[whIndex] / 2);
+    let newOtherSize = this.size.slice();
+    newOtherSize[whIndex] = this.size[whIndex] - newMySize[whIndex];
 
-    if (this.parent !== null && this.parent.splitDirection == SplitDirection.horizontal) {
-      let newChild = new Pane(this.parent, this.width, newOtherHeight);
+    if (this.parent !== null && this.parent.splitDirection == splitDirection) {
+      let newChild = new Pane(this.parent, newOtherSize);
       let position = this.parent.indexOf(this);
-      console.info('position is ' + position);
       this.parent.insertChild(newChild, position + 1);
-      this.height = newMyHeight;
+      this.size = newMySize;
       this.parent.reload();
     } else {
-      let newParent = new Container(this.parent, this.width, this.height, SplitDirection.horizontal);
-      let newChild = new Pane(newParent, this.width, newOtherHeight);
-      this.height = newMyHeight;
+      let newParent = new Container(this.parent, this.size, splitDirection);
+      let newChild = new Pane(newParent, newOtherSize);
+      this.size = newMySize;
       if (this.parent !== null)
         this.parent.replaceChild(this, newParent);
       this.changeParent(newParent);
@@ -115,34 +106,11 @@ class Container {
     }
   }
 
-  splitVertically() {
-    let newMyWidth = parseInt(this.width / 2);
-    let newOtherWidth = this.width - newMyWidth;
-
-    if (this.parent !== null && this.parent.splitDirection == SplitDirection.vertical) {
-      let newChild = new Pane(this.parent, newOtherWidth, this.height);
-      let position = this.parent.indexOf(this);
-      console.info('position is ' + position);
-      this.parent.insertChild(newChild, position + 1);
-      this.width = newMyWidth;
-      this.parent.reload();
-    } else {
-      let newParent = new Container(this.parent, this.width, this.height, SplitDirection.vertical);
-      let newChild = new Pane(newParent, newOtherWidth, this.height);
-      this.width = newMyWidth;
-      if (this.parent !== null)
-        this.parent.replaceChild(this, newParent);
-      this.changeParent(newParent);
-
-      newParent.appendChild(this);
-      newParent.appendChild(newChild);
-      newParent.reload();
-    }
-  }
-
-  resizeChildHorizontally(child, size) {
-    if (this.splitDirection !== SplitDirection.vertical) {
-      if (this.parent !== null) this.parent.resizeChildHorizontally(this, size);
+  resizeChild(child, size, direction) {
+    const whIndex = direction === SplitDirection.horizontal? 0: 1;
+    // ここの条件わかりにくいので直す（そもそもresizeでsplitDirectionってのがよくわからん）
+    if (this.splitDirection === null || this.splitDirection === direction) {
+      if (this.parent !== null) this.parent.resizeChild(this, size, direction);
       return;
     }
     if (this.children.length < 2) {
@@ -151,45 +119,17 @@ class Container {
 
     const index = this.children.indexOf(child);
     if (index === this.children.length - 1) {
-      // most right pane has different direction
-      if (this.children[index - 1].width + size > 0 &&
-          this.children[index].width - size > 0) {
-        this.children[index - 1].width += size;
-        this.children[index].width -= size;
+      // most bottom/right pane has different direction
+      if (this.children[index - 1].size[whIndex] + size > 0 &&
+          this.children[index].size[whIndex] - size > 0) {
+        this.children[index - 1].size[whIndex] += size;
+        this.children[index].size[whIndex] -= size;
       }
     } else {
-      if (this.children[index].width + size > 0 &&
-          this.children[index + 1].width - size > 0) {
-        this.children[index].width += size;
-        this.children[index + 1].width -= size;
-      }
-    }
-
-    rootContainer.reload();
-  }
-
-  resizeChildVertically(child, size) {
-    if (this.splitDirection !== SplitDirection.horizontal) {
-      if (this.parent !== null) this.parent.resizeChildVertically(this, size);
-      return;
-    }
-    if (this.children.length < 2) {
-      console.error('invalid child size: ' + this.children.length);
-    }
-
-    const index = this.children.indexOf(child);
-    if (index === this.children.length - 1) {
-      // most right pane has different direction
-      if (this.children[index - 1].height + size > 0 &&
-          this.children[index].height - size > 0) {
-        this.children[index - 1].height += size;
-        this.children[index].height -= size;
-      }
-    } else {
-      if (this.children[index].height + size > 0 &&
-          this.children[index + 1].height - size > 0) {
-        this.children[index].height += size;
-        this.children[index + 1].height -= size;
+      if (this.children[index].size[whIndex] + size > 0 &&
+          this.children[index + 1].size[whIndex] - size > 0) {
+        this.children[index].size[whIndex] += size;
+        this.children[index + 1].size[whIndex] -= size;
       }
     }
 
@@ -197,52 +137,35 @@ class Container {
   }
 
   reload() {
-    // propagate its size to the children
-    if (this.splitDirection === SplitDirection.horizontal) {
-      let totalHeight = 0;
-      for (var i = 0; i < this.children.length; i++)
-        totalHeight += this.children[i].height;
-      if (this.height !== totalHeight) {
-        let resizeRatio = this.height / totalHeight;
-        totalHeight = 0;
-        for (var i = 0; i < this.children.length - 1; i++) {
-          this.children[i].height = parseInt(this.children[i].height * resizeRatio);
-          totalHeight += this.children[i].height;
-        }
-        this.children[this.children.length - 1].height = this.height - totalHeight;
-      }
-      for (var i = 0; i < this.children.length; i++) {
-        this.children[i].width = this.width;
-        this.children[i].reload();
-      }
-    } else if (this.splitDirection === SplitDirection.vertical) {
-      let totalWidth = 0;
-      for (var i = 0; i < this.children.length; i++)
-        totalWidth += this.children[i].width;
-      if (this.width !== totalWidth) {
-        let resizeRatio = this.width / totalWidth;
-        totalWidth = 0;
-        for (var i = 0; i < this.children.length - 1; i++) {
-          this.children[i].width = parseInt(this.children[i].width * resizeRatio);
-          totalWidth += this.children[i].width;
-        }
-        this.children[this.children.length - 1].width = this.width - totalWidth;
-      }
-      for (var i = 0; i < this.children.length; i++) {
-        this.children[i].height = this.height;
-        this.children[i].reload();
-      }
+    if (this.splitDirection === null) {
+      for (var i = 0; i < this.children.length; i++) this.children[i].reload();
     }
 
+    const fixedAxis = this.splitDirection === SplitDirection.horizontal? 0: 1;
+    const movableAxis = this.splitDirection === SplitDirection.horizontal? 1: 0;
+    // propagate its size to the children
+    let totalHaba = 0;
+    for (var i = 0; i < this.children.length; i++)
+      totalHaba += this.children[i].size[movableAxis];
+    if (this.size[movableAxis] !== totalHaba) {
+      let resizeRatio = this.size[movableAxis] / totalHaba;
+      totalHaba = 0;
+      for (var i = 0; i < this.children.length - 1; i++) {
+        this.children[i].size[movableAxis] = parseInt(this.children[i].size[movableAxis] * resizeRatio);
+        totalHaba += this.children[i].size[movableAxis];
+      }
+      this.children[this.children.length - 1].size[movableAxis] = this.size[movableAxis] - totalHaba;
+    }
     for (var i = 0; i < this.children.length; i++) {
+      this.children[i].size[fixedAxis] = this.size[fixedAxis];
       this.children[i].reload();
     }
   }
 }
 
 class Pane extends Container {
-  constructor(parent, width, height) {
-    super(parent, width, height, null);
+  constructor(parent, size) {
+    super(parent, size, null);
 
     this.div = document.createElement('div');
     document.getElementById('container').appendChild(this.div);
@@ -268,8 +191,8 @@ class Pane extends Container {
 
   reload() {
     const position = this.getPosition();
-    this.div.style.gridColumn = position[0] + '/' + (position[0] + this.width);
-    this.div.style.gridRow = position[1] + '/' + (position[1] + this.height);
+    this.div.style.gridColumn = position[0] + '/' + (position[0] + this.size[0]);
+    this.div.style.gridRow = position[1] + '/' + (position[1] + this.size[1]);
   }
 
   onLoad() {
@@ -284,11 +207,11 @@ class Pane extends Container {
     if (this.command_triggered) {
       if (event.key === '"') {
         console.info('split horizontally');
-        this.splitHorizontally();
+        this.split(SplitDirection.horizontal);
         this.command_triggered = false;
       } else if (event.key === '%') {
         console.info('split vertically');
-        this.splitVertically();
+        this.split(SplitDirection.vertical);
         this.command_triggered = false;
       } else if (event.key === 'd') {
         console.info('remove');
@@ -297,19 +220,19 @@ class Pane extends Container {
         this.command_triggered = false;
       } else if (event.key === 'ArrowLeft') {
         console.info('left');
-        this.parent.resizeChildHorizontally(this, -1);
+        this.parent.resizeChild(this, -1, SplitDirection.horizontal);
         this.command_triggered = false;
       } else if (event.key === 'ArrowRight') {
         console.info('right');
-        this.parent.resizeChildHorizontally(this, 1);
+        this.parent.resizeChild(this, 1, SplitDirection.horizontal);
         this.command_triggered = false;
       } else if (event.key === 'ArrowUp') {
         console.info('up');
-        this.parent.resizeChildVertically(this, -1);
+        this.parent.resizeChild(this, -1, SplitDirection.vertical);
         this.command_triggered = false;
       } else if (event.key === 'ArrowDown') {
         console.info('down');
-        this.parent.resizeChildVertically(this, 1);
+        this.parent.resizeChild(this, 1, SplitDirection.vertical);
         this.command_triggered = false;
       }
     }
@@ -325,8 +248,8 @@ window.onload = function () {
   wrapper.style.gridTemplateColumns = 'repeat(' + colCount + ', ' + 100.0 / colCount + '%)';
 
   // rootContainer = new Pane(null, colCount, rowCount);
-  rootContainer = new Container(null, colCount, rowCount, null);
-  let initialPane = new Pane(rootContainer, colCount, rowCount);
+  rootContainer = new Container(null, [colCount, rowCount], null);
+  let initialPane = new Pane(rootContainer, [colCount, rowCount]);
   rootContainer.appendChild(initialPane);
   rootContainer.reload();
 }
